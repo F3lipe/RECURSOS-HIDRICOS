5df3ee4e6aab52eae474dcc988c69ec463c49f1a
Aluno <Aluno@LAB02-PC12>
2019-07-16 11:00:43 -0300

    Felipe 16/07

---
+++ recursoshidricos/src/br/edu/ifpb/esperanca/daw2/RecursosHidricos/Bean/UsuarioBean.java
@@ -0,0 +1,78 @@
+package br.edu.ifpb.esperanca.daw2.RecursosHidricos.Bean;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.edu.ifpb.esperanca.daw2.RecursosHidricos.entities.Usuario;
+import br.edu.ifpb.esperanca.daw2.RecursosHidricos.services.UsuarioService;
+
+
+@ViewScoped
+@Named
+public class UsuarioBean implements Serializable {
+
+	@Inject
+	private UsuarioService service;
+	protected Usuario entidade;
+	protected Collection<Usuario> entidades;
+
+	public UsuarioBean() {
+
+	}
+
+	@PostConstruct
+	public void init() {
+		entidade = newEntidade();
+		entidades = getService().getAll();
+	}
+
+	public void remove(Usuario entidade) {
+		getService().remove(entidade);
+		limpar();
+	}
+
+	public Usuario getEntidade() {
+		return entidade;
+	}
+
+	public void setEntidade(Usuario entidade) {
+		this.entidade = entidade;
+	}
+
+	public Collection<Usuario> getEntidades() {
+		return entidades;
+	}
+
+	public void setEntidades(Collection<Usuario> entidades) {
+		this.entidades = entidades;
+	}
+
+	public void save() {
+		getService().save(entidade);
+		limpar();
+	}
+
+	public void editar(Long id) {
+		this.getEntidade().setId(id);
+		save();
+	}
+
+	public void limpar() {
+		entidades = getService().getAll();
+		entidade = newEntidade();
+	}
+
+	protected Usuario newEntidade() {
+		return new Usuario();
+	}
+
+	public UsuarioService getService() {
+		return service;
+	}
+
+}
